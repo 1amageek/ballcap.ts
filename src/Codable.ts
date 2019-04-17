f517e8baf5313744e5a96e5e09e22bd5977f964f
1amageek <tmy0x3@icloud.com>
2019-04-17 18:03:45 +0900

    fix Field

--- src/Codable.ts
+++ src/Codable.ts
@@ -2,14 +2,6 @@ import { Modelable } from "./Modelable"
 import "reflect-metadata"
 
 export const CodableSymbol = Symbol("Codable")
-// export const Codable = <T extends Modelable>(condingKeys: { [field: string]: {new(): T} }) => {
-//     return (fn: new () => Modelable) => class extends fn {
-//         constructor() {
-//             super()
-//             Reflect.defineMetadata(CodableSymbol, condingKeys, this)
-//         }
-//     } as any
-// }
 
 export const Codable = <T extends Modelable>(type: {new(): T}, codingKey?: string) => {
     return <T extends Modelable>(target: T, fieldKey: string) => {
