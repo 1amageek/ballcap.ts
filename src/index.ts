ba4ea62fe961934388bc6c1c19683c5edc8c2e94
1amageek <tmy0x3@icloud.com>
2019-04-20 19:40:51 +0900

    fix index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
 import * as firebase from 'firebase'
+
+/*
+ * Protocol
+ */
+import { Codable } from './Codable'
+import { DataRepresentable } from './DataRepresentable'
+import { Referenceable } from './Referenceable'
+import { DocumentType, Documentable } from './Documentable'
+
+export { Codable, DataRepresentable, Referenceable, DocumentType, Documentable }
+
+/*
+ * Class
+ */
 import { Field } from './Field'
 import { File } from './File'
 import { Model } from './Model'
@@ -6,15 +20,6 @@ import { Doc } from './Document'
 import { Collection } from './Collection'
 import { Batch } from './Batch'
 
-/*
- * Protocl
- */
-import { Codable } from './Codable'
-import { DataRepresentable } from './DataRepresentable'
-import { Referenceable } from './Referenceable'
-import { Documentable } from './Documentable'
-
-export { Codable, DataRepresentable, Referenceable, Documentable}
 export { Field, File, Model, Doc, Collection, Batch }
 
 export let firestore: firebase.firestore.Firestore
