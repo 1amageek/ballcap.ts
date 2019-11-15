490fcea5f68ce23e8c1f563db2726ac924d724ac
1amageek <tmy0x3@icloud.com>
2019-11-15 16:11:44 +0900

    Fix jest test

---
+++ admin/jest.config.js
@@ -0,0 +1,9 @@
+const packageJson = require("./package.json");
+
+module.exports = {
+  name: packageJson.name,
+  displayName: packageJson.name,
+  rootDir: "./",
+  preset: "ts-jest",
+  testEnvironment: "node"
+};
