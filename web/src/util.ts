467c83aea47a64e57e557f86955730c369094e7c
1amageek <tmy0x3@icloud.com>
2020-03-26 18:02:15 +0900

    Fix

--- web/src/util.ts
+++ web/src/util.ts
@@ -1,5 +1,10 @@
 
 export const isDocumentReference = (value: any): boolean => {
-	if (!value) { return false}
-	return value.hasOwnProperty('_key') && value.hasOwnProperty('firestore')
+	if (!value) { return false }
+	return (value.hasOwnProperty('_key') && value.hasOwnProperty('firestore')) ||
+		(Object.keys(value).length === 2 && value.hasOwnProperty('projectId') && value.hasOwnProperty('path'))
+}
+
+export const isOption = (value: any): boolean => {
+	return Object.keys(value).length === 1 && value.hasOwnProperty('convertDocumentReference')
 }
